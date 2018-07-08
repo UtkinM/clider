48b754738e7eb8e1e6cf80e493beb3c9a13633e0
m.utkin <frontend.utkin@gmail.com>
2018-07-08 13:36:38 +0400

    added style

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,6 +17,25 @@ const Repair = styled.div`
     }
 `
 
+const LightText = styled.div`
+    color: #ffffff;
+    font-size: 18px;
+    font-weight: 300;
+    line-height: 28px;
+    margin-top: 54px;
+    padding-bottom: 32px;
+`
+const CallBtn = styled.button`
+    width: 247px;
+    height: 67px;
+    background-color: #ffa14b;
+    border: none;
+    border-radius: 30px;
+    color: #ffffff;
+    font-size: 18px;
+    font-weight: bold;
+`
+
 class Main extends React.Component {
     render(){
         return (
@@ -26,6 +45,14 @@ class Main extends React.Component {
                             Качественный ремонт
                             <span>iphone за 35 минут и гарантия 1 год</span>
                         </Repair>
+                        <LightText>
+                            Оставьте заявку на бесплатную диагностику без очереди,
+                            и получите защитное стекло, стоимостью 1000 рублей,
+                            с установкой в подарок!
+                        </LightText>
+                        <CallBtn>
+                            Отправить заявку!
+                        </CallBtn>
                     </Col>
                     <Col lg={6} lgOffset={1}>
 
